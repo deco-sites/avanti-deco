13cd6e48adbe2caa16459e352ab5cf2373f100e7
Bruno Monteiro <brunobapmon@gmail.com>
2023-09-14 09:37:32 -0300

    feat: pass headers at graphql request

--- sections/OrderFormReader/OrderFormReader.tsx
+++ sections/OrderFormReader/OrderFormReader.tsx
@@ -1,23 +1,24 @@
 import type { SectionProps } from "$live/mod.ts";
 import { createGraphqlClient } from "apps/utils/graphql.ts";
 
-export async function loader() {
+export async function loader(_: any, req: Request) {
   const io = createGraphqlClient({
     endpoint: `https://avantivtexio.vtexcommercestable.com.br/api/io/_v/private/graphql/v1`,
+    headers: req.headers,
   });
   try {
     const data = await io.query({
       query: `query OrderForm {
-  orderForm @context(provider: "vtex.checkout-graphql") {
-    id
-    clientProfileData {
-      email
-    }
-    items {
-      id
-    }
-  }
-}`,
+                orderForm @context(provider: "vtex.checkout-graphql") {
+                  id
+                  clientProfileData {
+                    email
+                  }
+                  items {
+                    id
+                  }
+                }
+              }`,
     });
     return { data };
   } catch (e) {
@@ -35,7 +36,7 @@ export default function OrderFormReader({ data }: SectionProps<typeof loader>) {
         {data ? (
           <ul>
             <li>E-mail: {data.orderForm.clientProfileData.email}</li>
-            <li>Itens: {data.orderForm.items.map((item) => item.id)}</li>
+            <li>Itens: {JSON.stringify(data.orderForm.items)}</li>
           </ul>
         ) : (
           "Não foi possível carregar"
